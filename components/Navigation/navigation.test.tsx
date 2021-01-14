8621bf7e1de14bc1071e1c0c6773eded067f1050
bartektelec <bartektelec@gmail.com>
2021-01-14 22:54:45 +0100

    fix: :green_heart: declare eva icons module

--- components/Navigation/navigation.test.tsx
+++ components/Navigation/navigation.test.tsx
@@ -1,16 +1,10 @@
 import * as React from 'react';
 import Navigation from './index';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 describe('Navigation component', () => {
   it('should render', () => {
     const { getByTestId } = render(<Navigation />);
     expect(getByTestId('nav')).toBeVisible();
-  });
-  it('should disappear when scroll down', () => {
-    const { getByTestId } = render(<Navigation />);
-    fireEvent.scroll(window, { y: 300 });
-
-    expect(getByTestId('nav')).toHaveStyle({ top: '-60px' });
   });
 });
