e691f65be08b5e05da26f1bac9339dbfab454e59
Bartlomiej Telec <bartlomiej.andrzej.telec@hurtigruten.com>
2021-08-21 21:57:48 +0200

    chore: :art: fix typos, og image for linkedin, update current empoyment status and tech

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -1,52 +1,57 @@
-import Container from '../Container/';
-import SkillCard from '../SkillCard/';
+import Container from "../Container/";
+import SkillCard from "../SkillCard/";
 
 export interface AboutProps {}
 
 const About: React.FC<AboutProps> = () => {
-  return (
-    <section id='about' className='mb-16'>
-      <Container>
-        <h2 className='font-marker text-4xl my-16'>
-          <span className='accented'>About me</span>
-        </h2>
-        <div className='mx-auto flex flex-col gap-8 text-center md:w-1/2'>
-          <h1 data-animate className='text-4xl'>
-            Hello, I'm Bartlomiej, a <strong>frontend developer</strong> based
-            in Oslo, Norway.
-          </h1>
-          <p data-animate>
-            I enjoy creating interactive, <strong>good-looking</strong> things
-            such as websites and applications.
-          </p>
-          <p data-animate>
-            I am a current student at <strong>Noroff Vocational</strong> in
-            Oslo, as well as I’m always learning new stuff in my spare time.
-          </p>
-        </div>
-        <div className='grid grid-cols-4 gap-12 sm:grid-cols-12 mt-16'>
-          <SkillCard
-            icon='code-outline'
-            title='Web development'
-            text='I specialize in converting graphical interface projects to code that results as fast, responsive, and accessible website or web app. '
-            tags='React, Vue.js, Next.js, JavaScript, TypeScript, Jest, GSAP'
-          />
-          <SkillCard
-            icon='brush-outline'
-            title='UI Design'
-            text='I always take care that my projects are designed with care and accessibiliy in mind for the best user experience. '
-            tags='WCAG, ARIA, Figma, Adobe XD, Photoshop'
-          />
-          <SkillCard
-            icon='smartphone-outline'
-            title='Other'
-            text='There are many ways to create an application, I enjoy picking up new tech and am able to do frontend and backend, as well as mobile development. '
-            tags='PWA, React Native, RWD, Node.js, Express, MongoDB'
-          />
-        </div>
-      </Container>
-    </section>
-  );
+	return (
+		<section id="about" className="mb-16">
+			<Container>
+				<h2 className="font-marker text-4xl my-16">
+					<span className="accented">About me</span>
+				</h2>
+				<div className="mx-auto flex flex-col gap-8 text-center md:w-1/2">
+					<h1 data-animate className="text-4xl">
+						Hello, I'm Bartlomiej, a <strong>frontend developer</strong> based
+						in Oslo, Norway.
+					</h1>
+					<p data-animate>
+						I enjoy creating interactive, <strong>good-looking</strong> things
+						such as websites and applications, as well as I’m always learning
+						new stuff in my spare time.
+					</p>
+					<p data-animate>
+						I am currently working as a <strong>Software Developer</strong> in
+						Hurtigruten AS.
+					</p>
+					<p data-animate>
+						I graduated <strong>Noroff Vocational School</strong> in Oslo with
+						an associate degree in Frontend development.
+					</p>
+				</div>
+				<div className="grid grid-cols-4 gap-12 sm:grid-cols-12 mt-16">
+					<SkillCard
+						icon="code-outline"
+						title="Web development"
+						text="I specialize in converting graphical interface projects to code that results as fast, responsive, and accessible website or web app. "
+						tags="React, Svelte, Vue.js, Next.js, JavaScript, TypeScript, Jest, GSAP"
+					/>
+					<SkillCard
+						icon="brush-outline"
+						title="UI Design"
+						text="I always take care that my projects are designed with care and accessibiliy in mind for the best user experience. "
+						tags="WCAG, ARIA, A11y, Figma, Adobe XD, Photoshop"
+					/>
+					<SkillCard
+						icon="smartphone-outline"
+						title="Other"
+						text="There are many ways to create an application, I enjoy picking up new tech and am able to do frontend and backend, as well as mobile development. "
+						tags="PWA, C#.NET, WebSockets, React Native, RWD, Node.js, Express, MongoDB"
+					/>
+				</div>
+			</Container>
+		</section>
+	);
 };
 
 export default About;
--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,19 +1,19 @@
-import Container from '../Container/';
+import Container from "../Container/";
 
 export interface FooterProps {}
 
 const Footer: React.FC<FooterProps> = () => {
-  return (
-    <footer className='mt-48 bg-gray-100 text-gray-800 py-4'>
-      <Container className='gap-2 flex flex-col justify-center items-center text-center'>
-        <p className='text-xs'>
-          This site is built with 💙 using TypeScript, Next.js, TailwindCSS and
-          GSAP.
-        </p>
-        <p>2021 © bartektelec.com</p>
-      </Container>
-    </footer>
-  );
+	return (
+		<footer className="mt-48 bg-gray-100 text-gray-800 py-4">
+			<Container className="gap-2 flex flex-col justify-center items-center text-center">
+				<p className="text-xs">
+					This site is built with 💙 using TypeScript, Next.js, TailwindCSS and
+					GSAP.
+				</p>
+				<p>2021 © btelec.no</p>
+			</Container>
+		</footer>
+	);
 };
 
 export default Footer;
